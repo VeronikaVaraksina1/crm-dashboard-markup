58a106332c3941e3bd62ef260454c9672925d58a
VeronikaVaraksina1 <plaguemoon@gmail.com>
2024-11-23 18:37:46 +0200

    customers styles in process

--- gulpfile.js
+++ gulpfile.js
@@ -14,110 +14,115 @@ const svgSprite = require("gulp-svg-sprite");
 const cssfont64 = require("gulp-cssfont64");
 
 function fontsConvert() {
-  return src(["assets/fonts/*.woff", "assets/fonts/*.woff2"])
-    .pipe(cssfont64())
-    .pipe(dest("dist/styles/"))
-    .pipe(browserSync.stream());
+    return src(["assets/fonts/*.woff", "assets/fonts/*.woff2"])
+        .pipe(cssfont64())
+        .pipe(dest("dist/styles/"))
+        .pipe(browserSync.stream());
 }
 
 function images() {
-  return src("assets/i/**/*")
-    .pipe(newer("dist/i/"))
-    .pipe(webp({ quality: 80 }))
+    return src("assets/i/**/*")
+        .pipe(newer("dist/i/"))
+        .pipe(webp({ quality: 80 }))
 
-    .pipe(src("assets/i/**/*"))
-    .pipe(newer("dist/i/"))
-    .pipe(imagemin())
+        .pipe(src("assets/i/**/*"))
+        .pipe(newer("dist/i/"))
+        .pipe(imagemin())
 
-    .pipe(dest("dist/i/"))
-    .pipe(browserSync.stream({ once: true }));
+        .pipe(dest("dist/i/"))
+        .pipe(browserSync.stream({ once: true }));
 }
 
 function sprite() {
-  return src("assets/i/icons/*.svg")
-    .pipe(
-      svgSprite({
-        mode: {
-          symbol: {
-            sprite: "../sprite.svg",
-            render: {
-              scss: {
-                dest: "../../../sass/_sprite.scss",
-                template: "assets/sass/templates/_sprite_template.scss",
-              },
-            },
-          },
-        },
-      })
-    )
-    .pipe(dest("assets/i/sprite/"));
+    return src("assets/i/icons/*.svg")
+        .pipe(
+            svgSprite({
+                mode: {
+                    symbol: {
+                        sprite: "../sprite.svg",
+                        render: {
+                            scss: {
+                                dest: "../../../sass/_sprite.scss",
+                                template:
+                                    "assets/sass/templates/_sprite_template.scss",
+                            },
+                        },
+                    },
+                },
+            })
+        )
+        .pipe(dest("assets/i/sprite/"));
 }
 
 function pugToHtml() {
-  return src(["assets/pug/*.pug", "!assets/pug/_*.pug"])
-    .pipe(pug({ pretty: true }))
-    .pipe(dest("dist/"))
-    .pipe(browserSync.stream());
+    return src(["assets/pug/*.pug", "!assets/pug/_*.pug"])
+        .pipe(pug({ pretty: true }))
+        .pipe(dest("dist/"))
+        .pipe(browserSync.stream());
 }
 
 function styles() {
-  return src(["assets/sass/main_global.scss"])
-    .pipe(autoprefixer({ overrideBrowserslist: ["last 3 version"] }))
-    .pipe(concat("main_global.min.css"))
-    .pipe(scss({ outputStyle: "compressed" }))
-    .pipe(dest("dist/styles/"))
-    .pipe(browserSync.stream({ match: "**/*.css" }));
+    return src(["assets/sass/main_global.scss"])
+        .pipe(autoprefixer({ overrideBrowserslist: ["last 3 version"] }))
+        .pipe(concat("main_global.min.css"))
+        .pipe(scss({ outputStyle: "compressed" }))
+        .pipe(dest("dist/styles/"))
+        .pipe(browserSync.stream({ match: "**/*.css" }));
 }
 
 function scripts() {
-  return src(["assets/**/*.js", "!assets/js/main.min.js"])
-    .pipe(concat("main.min.js"))
-    .pipe(uglify())
-    .pipe(dest("dist/js/"))
-    .pipe(browserSync.stream());
+    return src(["assets/**/*.js", "!assets/js/main.min.js"])
+        .pipe(concat("main.min.js"))
+        .pipe(uglify())
+        .pipe(dest("dist/js/"))
+        .pipe(browserSync.stream());
 }
 
 function purgeCss() {
-  return src(["dist/styles/main_global.min.css"])
-    .pipe(
-      purgecss({
-        content: ["assets/**/*.html", "assets/**/*.pug", "assets/**/*.js"],
-      })
-    )
-    .pipe(dest("dist/styles/"));
+    return src(["dist/styles/main_global.min.css"])
+        .pipe(
+            purgecss({
+                content: [
+                    "assets/**/*.html",
+                    "assets/**/*.pug",
+                    "assets/**/*.js",
+                ],
+            })
+        )
+        .pipe(dest("dist/styles/"));
 }
 
 function watching() {
-  watch(["assets/sass/**/*.scss"], styles);
-  watch(["assets/i/"], images);
-  watch(["assets/js/main.js"], scripts);
-  watch(["assets/pug/**/*.pug"], pugToHtml);
-  watch(["assets/**/*.html"]).on("change", browserSync.reload);
+    watch(["assets/sass/**/*.scss"], styles);
+    watch(["assets/i/"], images);
+    watch(["assets/js/main.js"], scripts);
+    watch(["assets/pug/**/*.pug"], pugToHtml);
+    watch(["assets/**/*.html"]).on("change", browserSync.reload);
 }
 
 function browsersync() {
-  browserSync.init({
-    server: {
-      baseDir: "dist",
-    },
-  });
+    browserSync.init({
+        server: {
+            baseDir: "dist",
+        },
+    });
 }
 
 function cleanDist() {
-  return src("build/**/*").pipe(clean());
+    return src("build/**/*").pipe(clean());
 }
 
 function copySprite() {
-  return src("assets/i/sprite/sprite.svg").pipe(dest("dist/i/sprite/"));
+    return src("assets/i/sprite/sprite.svg").pipe(dest("dist/i/sprite/"));
 }
 
 function building() {
-  return src(
-    ["dist/styles/**/*", "dist/js/**/*", "dist/**/*.html", "dist/i/**/*"],
-    {
-      base: "dist",
-    }
-  ).pipe(dest("build/"));
+    return src(
+        ["dist/styles/**/*", "dist/js/**/*", "dist/**/*.html", "dist/i/**/*"],
+        {
+            base: "dist",
+        }
+    ).pipe(dest("build/"));
 }
 
 exports.fontsConvert = fontsConvert;
@@ -133,10 +138,10 @@ exports.watching = watching;
 exports.generateSprite = series(sprite, copySprite);
 exports.build = series(cleanDist, building);
 exports.default = parallel(
-  pugToHtml,
-  styles,
-  images,
-  scripts,
-  watching,
-  browsersync
+    pugToHtml,
+    styles,
+    images,
+    scripts,
+    watching,
+    browsersync
 );
